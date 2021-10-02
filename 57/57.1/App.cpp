d69047fed8c826cf730f3368b58d489fb768dff4
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-02 19:58:22 +0300

    Add 57.1

---
+++ 57/57.1/App.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::cout << "// Enter your full name.\n"
+               "> ";
+  std::string name;
+  std::getline(std::cin, name);
+  std::cout << '\n';
+
+  std::cout << "// Enter your age.\n"
+               "> ";
+  uint16_t age;
+  std::cin >> age;
+  std::cout << '\n';
+
+  double livedYears = static_cast<double>(age) / name.length();
+
+  std::cout << "You've lived " << livedYears << " years for each letter in your name.\n";
+  std::cout << '\n';
+  return 0;
+}
