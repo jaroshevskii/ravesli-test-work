0b08291642c61e42b70b5fee4ba053d1651771c8
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-02 21:22:53 +0300

    Add 58.2

---
+++ 58/58.2/App.cpp
@@ -0,0 +1,12 @@
+enum MonsterType {
+  MONSTER_OGRE,
+  MONSTER_GOBLIN,
+  MONSTER_SKELETON,
+  MONSTER_ORC,
+  MONSTER_TROLL
+};
+
+int main() {
+  MonsterType monsterType(MONSTER_OGRE);
+  return 0;
+}
