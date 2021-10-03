536ed20f50df014e76eab8d3b2acd61a3b556fe1
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-03 23:48:45 +0300

    Add 67.2

---
+++ 67/67.2/App.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  char letter = 'a';
+  while (letter <= 'z') {
+    std::cout << letter << ": " << static_cast<int>(letter)<< "\n";
+    
+    ++letter;
+  }
+  std::cout << '\n';
+  return 0;
+}
