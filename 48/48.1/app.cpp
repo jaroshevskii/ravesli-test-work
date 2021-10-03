091246828a928b41ea58c8425184b68858976e36
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-10-03 16:10:41 +0300

    Update 48.1

--- 48/48.1/app.cpp
+++ 48/48.1/app.cpp
@@ -6,24 +6,24 @@ int main() {
   int16_t smaller;
   std::cin >> smaller;
   std::cout << '\n';
-  
+
   std::cout << "// Enter a larger number.\n"
                "> ";
   int16_t larger;
   std::cin >> larger;
-  std::cout << '\n'; 
-  
+  std::cout << '\n';
+
   if (smaller > larger) {
     std::cout << "We change the numbers.\n";
     std::cout << '\n';
-    
+
     int16_t temp = smaller;
     smaller = larger;
     larger = temp;
   }
-  
-  std::cout << "Smaller: " << smaller << '\n'
-            << "Larger: " << larger << '\n';
+
+  std::cout << "Smaller: " << smaller << "\n"
+            << "Larger: " << larger << "\n";
   std::cout << '\n';
   return 0;
 }
